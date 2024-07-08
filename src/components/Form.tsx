483c66ae69445e26b98977077c81311975dc80cd
Facundo Grondona <facundo.grondona.02@gmail.com>
2024-07-08 17:30:10 -0300

    Mostramos los inputs, coordinamos el state de los datos para que se unifique

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,52 @@
 import { categories } from "../data/categories"
-
+import { useState } from "react"
 
 
 export default function Form() {
+    const[activity, setActivity] = useState({
+      category: 1,
+      name: "",
+      calories:0
+    })
+
+
+const handleChange = (e)=>{
+
+    setActivity({
+      ...activity,
+        [e.target.id]: e.target.value
+    })
+
+}
+
   return (
     <form className="space-y-5 p-10 bg-white shadow rounded-lg">
     <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">Categoria:</label>
-        <select className="border border-slate-300 p-2 rounded-lg bg-white" id="category">
+        <select
+         className="border border-slate-300 p-2 rounded-lg bg-white" 
+         id="category" 
+         value={activity.category}
+         onChange={handleChange}
+         >
         
         {categories.map((cat)=> (
 
-           <option key={cat.id} value={cat.id}>{cat.name}</option>
+           <option key={cat.id} >{cat.name}</option>
         ))}
-        
         </select>
     </div>
 
     <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <label htmlFor="name" className="font-bold">Actividad:</label>
         <input 
         type="text" 
-        id="activity" 
+        id="name" 
         className="border-slate-300 border p-2 rounded-lg" 
         placeholder="Ej: Jugo de naranja, ejercicio, manzana, etc..."
+        value={activity.name}
+        onChange={handleChange}
+
         />
     </div>
 
@@ -34,6 +57,9 @@ export default function Form() {
         id="calories" 
         className="border-slate-300 border p-2 rounded-lg" 
         placeholder="Ej: 200, 500, etc..."
+        value={activity.calories}
+        onChange={handleChange}
+
         />
     </div>
 
